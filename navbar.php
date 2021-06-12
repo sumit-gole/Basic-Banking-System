def6117dbe5a966933329fb56e4f06671b300172
Sumit Gole <67162604+sumit-gole@users.noreply.github.com>
2021-06-12 16:16:49 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,30 @@
+
+<div class="maincontainer"  style="min-height : 100vh; ">
+    <section class='navbarsection'>
+        <div class='left'>
+            <div>
+                <h1>The Sparks Bank</h1>
+            </div>
+        </div>
+        <div class='right'>
+            <ul class='socials'>
+                <li>
+                    <a href='index.php'>Home</a>
+                </li>
+                <li>
+                    <a href='viewallcustomers.php'>User</a>
+                </li>
+                <li>
+                    <a href='transfermoney.php'>Transaction</a>
+                </li>
+                <li>
+                    <a href='transactionhistory.php'>History</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</main>
+
+
+
+
